744a363adc503cae4b5b3ae941f489eed8b6c2bb
y129may9th <y129may9th@gmail.com>
2019-05-06 23:19:40 +0900

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "create Users"
+User.create!(
+    email: 'cat1@example.com',
+    password: 'password',
+    name: 'オリバー',
+    self_introduction: 'オリバーです',
+    sex: 0,
+    species: '白猫',
+    img_name: open("#{Rails.root}/db/dummy_img/cat1.jpg")
+)
+User.create!(
+    email: 'cat2@example.com',
+    password: 'password',
+    name: 'ハリー',
+    self_introduction: 'ハリーです',
+    sex: 0,
+    species: '赤毛長毛猫',
+    img_name: open("#{Rails.root}/db/dummy_img/cat2.jpg")
+)
+User.create!(
+    email: 'cat3@example.com',
+    password: 'password',
+    name: 'オリヴィア',
+    self_introduction: 'オリヴィアです',
+    sex: 1,
+    species: '黒猫',
+    img_name: open("#{Rails.root}/db/dummy_img/cat3.jpg")
+)
+User.create!(
+    email: 'cat4@example.com',
+    password: 'password',
+    name: 'リリー',
+    self_introduction: 'リリーです',
+    sex: 1,
+    species: 'マーブル',
+    img_name: open("#{Rails.root}/db/dummy_img/cat4.jpg")
+)
+
