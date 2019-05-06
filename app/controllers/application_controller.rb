27cdeb00430f98f82a42ccf76387edd7ae396ae6
y129may9th <y129may9th@gmail.com>
2019-05-06 18:30:38 +0900

    View sign_up sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
@@ -6,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name self_introduction sex species img_name))
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name self_introduction sex img_name))
   end
 
 end
