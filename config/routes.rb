2d73f6f9c96da54d3f0b4fa9a5c78c042714f0bd
y129may9th <y129may9th@gmail.com>
2019-05-09 23:58:15 +0900

    Add reactions sontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => :registrations }
-  root "top#index"
-  
+  root "top#index"  
   resources :users, only: %i(index show)
+
+  resources :reactions, only: %i(create)
 end
 
