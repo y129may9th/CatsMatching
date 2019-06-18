4c3e5a1ac61afd479ceebede913e782b971840a5
y129may9th <y129may9th@gmail.com>
2019-06-18 22:36:39 +0900

    Fix carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,12 @@
 if Rails.env.production?
-    CarrierWave.configure do |config|
-        config.fog_credentials = {
-          provider: 'AWS',
-          aws_access_key_id: ENV['AKIAX3TRONJEXGS6VO4Q'],
-          aws_secret_access_key: ENV['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'],
-          region: ENV['ap-northeast-1'],
-          path_style: true
-        }
-        config.fog_public = true
-        config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-      
-        config.remove_previously_stored_files_after_update = false
-      
-        config.fog_directory = ENV['bucket-name-xxx']
-        config.asset_host = ENV['bucket-name-xxx']
-      end
-      # 日本語の文字化けを防ぐ
-      CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
